63d0c471831aa0a5c3b627b01d3ecc0b0847aa23
Mike Stipicevic <stipredirect@gmail.com>
2012-05-12 21:30:09 -0700

    compiles but doesn't work

--- blarg/blarg.go
+++ blarg/blarg.go
@@ -1,19 +1,27 @@
 package blarg
 
 import (
-    "io"
     "net/http"
     "pat"
+    "mustache"
+    "bytes"
 )
 
 // hello world, the web server
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-    io.WriteString(w, "hello, "+req.URL.Query().Get(":name")+"!\n")
+func HelloServer(w http.ResponseWriter, req *http.Request, tmpl *mustache.Template) {
+    var buf bytes.Buffer;
+    tmpl.Render (map[string]string { "c": req.URL.Query().Get(":name")}, &buf)
+    buf.WriteTo(w)
 }
 
 func init() {
+    tmpl,_ := mustache.ParseString("hello {{c}}!\n")
+    tmplhdlr:= func(w http.ResponseWriter, req *http.Request) {
+      HelloServer(w, req, tmpl)
+    }
+
     m := pat.New()
-    m.Get("/hello/:name", http.HandlerFunc(HelloServer))
+    m.Get("/hello/:name", http.HandlerFunc(tmplhdlr))
 
     // Register this pat with the default serve mux so that other packages
     // may also be exported. (i.e. /debug/pprof/*)
