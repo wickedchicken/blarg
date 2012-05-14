cd1c451c05162b95ed8d0fb744c5208d5a902a57
Mike Stipicevic <stipredirect@gmail.com>
2012-05-13 18:51:36 -0700

    now renders a page based on mustache

--- blarg/blarg.go
+++ blarg/blarg.go
@@ -7,22 +7,35 @@ import (
     "io"
 )
 
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-    context := map[string]string { "c": req.URL.Query().Get(":name")}
-    l := mustache.Render("hello {{c}}!\n", context)
-    io.WriteString(w, l)
+func Index(w http.ResponseWriter, req *http.Request) {
+
+  bloginfo := map[string]string { "blog_name": "my blarg!" }
+  static_private := "static_private/"
+
+  h := mustache.RenderFile(static_private + "header.html.mustache", bloginfo)
+  io.WriteString(w, h)
+
+  //context := map[string]string { "c": req.URL.Query().Get(":name") }
+  context := map[string]string { "c": "yolo!" }
+  c := mustache.RenderFile(static_private + "splash.html.mustache", context)
+  io.WriteString(w, c)
+
+  timing := map[string]string { "render": "0.01s" }
+  f := mustache.RenderFile(static_private + "footer.html.mustache", timing)
+  io.WriteString(w, f)
 }
 
 func init() {
-    m := pat.New()
-    m.Get("/", http.HandlerFunc(Index))
-    m.Get("/list/:start", http.HandlerFunc(Index))
-    // m.Get("/rss", http.HandlerFunc(Rss))
-    // m.Get("/sitemap.xml", http.HandlerFunc(Sitemap))
-    m.Get("/item/:item", http.HandlerFunc(Lookup))
-    m.Get("/admin", http.HandlerFunc(Admin))
+  m := pat.New()
+  m.Get("/hello/:name", http.HandlerFunc(Index))
+  m.Get("/", http.HandlerFunc(Index))
+  //m.Get("/list/:start", http.HandlerFunc(Index))
+  // m.Get("/rss", http.HandlerFunc(Rss))
+  // m.Get("/sitemap.xml", http.HandlerFunc(Sitemap))
+  //m.Get("/item/:item", http.HandlerFunc(Lookup))
+  //m.Get("/admin", http.HandlerFunc(Admin))
 
-    // Register this pat with the default serve mux so that other packages
-    // may also be exported. (i.e. /debug/pprof/*)
-    http.Handle("/", m)
+  // Register this pat with the default serve mux so that other packages
+  // may also be exported. (i.e. /debug/pprof/*)
+  http.Handle("/", m)
 }
