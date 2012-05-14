2c44333bc8bedec3262c839a44f2c47ee6a2442f
Mike Stipicevic <stipredirect@gmail.com>
2012-05-13 18:02:56 -0700

    add static and static_private directories

--- blarg/blarg.go
+++ blarg/blarg.go
@@ -7,7 +7,6 @@ import (
     "io"
 )
 
-// hello world, the web server
 func HelloServer(w http.ResponseWriter, req *http.Request) {
     context := map[string]string { "c": req.URL.Query().Get(":name")}
     l := mustache.Render("hello {{c}}!\n", context)
@@ -16,7 +15,12 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 
 func init() {
     m := pat.New()
-    m.Get("/hello/:name", http.HandlerFunc(HelloServer))
+    m.Get("/", http.HandlerFunc(Index))
+    m.Get("/list/:start", http.HandlerFunc(Index))
+    // m.Get("/rss", http.HandlerFunc(Rss))
+    // m.Get("/sitemap.xml", http.HandlerFunc(Sitemap))
+    m.Get("/item/:item", http.HandlerFunc(Lookup))
+    m.Get("/admin", http.HandlerFunc(Admin))
 
     // Register this pat with the default serve mux so that other packages
     // may also be exported. (i.e. /debug/pprof/*)
