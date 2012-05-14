dab7055d1e7c8c012eedfeee4ad9e37fdfba9682
Mike Stipicevic <stipredirect@gmail.com>
2012-05-13 19:49:47 -0700

    now utilizes blobstore database

--- blarg/blarg.go
+++ blarg/blarg.go
@@ -7,9 +7,42 @@ import (
     "blackfriday"
     "bytes"
     "io"
+    "time"
+    "appengine"
+    "appengine/datastore"
 )
 
+type Post struct {
+    Title       string
+    Content     string
+    Postdate    time.Time
+    StickyUrl   string
+    Sidebar     bool
+}
+
 func Index(w http.ResponseWriter, req *http.Request) {
+
+  cd := appengine.NewContext(req)
+
+  p1 := Post{
+      Title:    "awesome post great job",
+      Content:  "chickens\n======\n\nchickens are *boss*. look at this:\n\n* chickens are tasty\n* chickens are not green\n* you too can be a chicken with focused thought",
+      Postdate: time.Now(),
+      StickyUrl: "",
+      Sidebar: false,
+  }
+
+  key, err := datastore.Put(cd, datastore.NewIncompleteKey(cd, "post", nil), &p1)
+  if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+  }
+
+  var p2 Post
+  if err = datastore.Get(cd, key, &p2); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+  }
 
   bloginfo := map[string]string { "blog_title": "my blarg!" }
   static_private := "static_private/"
@@ -17,15 +50,13 @@ func Index(w http.ResponseWriter, req *http.Request) {
   h := mustache.RenderFile(static_private + "header.html.mustache", bloginfo)
   io.WriteString(w, h)
 
-  astring := "chickens\n======\n\nchickens are *boss*. look at this:\n\n* chickens are tasty\n* chickens are not green\n* you too can be a chicken with focused thought"
-
   //context := map[string]string { "c": req.URL.Query().Get(":name") }
   //context := map[string]string { "c": "yolo!" }
   //c := mustache.RenderFile(static_private + "splash.html.mustache", context)
   //io.WriteString(w, c)
 
   //what
-  con := bytes.NewBuffer(blackfriday.MarkdownCommon(bytes.NewBufferString(astring).Bytes())).String()
+  con := bytes.NewBuffer(blackfriday.MarkdownCommon(bytes.NewBufferString(p2.Content).Bytes())).String()
   context := map[string]string { "c": con }
   c := mustache.RenderFile(static_private + "splash.html.mustache", context)
   io.WriteString(w, c)
