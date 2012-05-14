5fee50742d009ba99aee86eb0bea37cc25326574
Mike Stipicevic <stipredirect@gmail.com>
2012-05-13 19:20:27 -0700

    now can render markdown

--- blarg/blarg.go
+++ blarg/blarg.go
@@ -4,19 +4,29 @@ import (
     "net/http"
     "pat"
     "mustache"
+    "blackfriday"
+    "bytes"
     "io"
 )
 
 func Index(w http.ResponseWriter, req *http.Request) {
 
-  bloginfo := map[string]string { "blog_name": "my blarg!" }
+  bloginfo := map[string]string { "blog_title": "my blarg!" }
   static_private := "static_private/"
 
   h := mustache.RenderFile(static_private + "header.html.mustache", bloginfo)
   io.WriteString(w, h)
 
+  astring := "chickens\n======\n\nchickens are *boss*. look at this:\n\n* chickens are tasty\n* chickens are not green\n* you too can be a chicken with focused thought"
+
   //context := map[string]string { "c": req.URL.Query().Get(":name") }
-  context := map[string]string { "c": "yolo!" }
+  //context := map[string]string { "c": "yolo!" }
+  //c := mustache.RenderFile(static_private + "splash.html.mustache", context)
+  //io.WriteString(w, c)
+
+  //what
+  con := bytes.NewBuffer(blackfriday.MarkdownCommon(bytes.NewBufferString(astring).Bytes())).String()
+  context := map[string]string { "c": con }
   c := mustache.RenderFile(static_private + "splash.html.mustache", context)
   io.WriteString(w, c)
 
@@ -27,7 +37,6 @@ func Index(w http.ResponseWriter, req *http.Request) {
 
 func init() {
   m := pat.New()
-  m.Get("/hello/:name", http.HandlerFunc(Index))
   m.Get("/", http.HandlerFunc(Index))
   //m.Get("/list/:start", http.HandlerFunc(Index))
   // m.Get("/rss", http.HandlerFunc(Rss))
